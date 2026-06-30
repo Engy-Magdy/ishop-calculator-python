ee31ab6c6b3770a75816ae8b0d026899139ac5c6
Engy-Magdy <engymelad237@gmail.com>
2026-06-30 20:43:43 +0300

    Fix price input type and enhance user interaction
    Updated price input to accept float values and improved user prompts.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Greeting to inshop calculator
 print("""
+      
       ******Welcome to ishop calculator******
       """)
 #Saving the names of items and their prices
@@ -7,22 +8,26 @@ all_items=[]
 total_price=[]
 #Asking about number of items
 n=int(input("How many items are there in your basket today?\n"))
-print("\nLet's get to counting them........")
-#loops of the number of items
-for i in range(1,n+1):
+if n>0:
+  print("\nLet's get to counting them........")
+#loops of the number of items and their prices
+  for i in range(1,n+1):
     name=input(f"Please tell me the name of the item number {i}:\n")
     all_items.append(name)
-    price=int(input(f"What is the price of {name} $ ?\n"))
+    price=float(input(f"What is the price of {name} $ ?\n"))
     total_price.append(price)
  #List of all items
-list_items=input("Would you like to see your entire basket items ?").lower()
-if list_items=="yes":
-    print(all_items)
-#List of total price
-list_price=input("Would you like to see how much it will cost?\n").upper()
-if list_price=="YES":
-    print(f"Buying these items will cost:\n{sum(total_price)}")
+  list_items=input("Would you like to see your entire basket items ?\n").lower()
+  if list_items=="yes":
+      print(all_items)
+#List of all prices      
+  list_total_price=input("Would you to see how much it will cost?").upper()
+  if  list_total_price=="YES":
+      print(f"Buying these items will cost:\n{sum(total_price)}")
+  else:
+      input("Press enter to exit......")
 else:
-    input("Press enter to exit......")           
+    input("It seems that you do not want to buy something.Press enter to exit.....")        
 
+   
 
